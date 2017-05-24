a20d0dcbcfc5e6c1cebb99abbf3e8a29cb770f69
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2017-05-24 01:05:58 +0000

    final code for timestamp microservice

--- timestamp/server.js
+++ timestamp/server.js
@@ -1,10 +1,35 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+var months = [ "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December" ];
+
+function unix2natural(timestamp){
+  
+  var date = new Date(timestamp * 1000)
+  var month = months[date.getMonth()]
+  var day = date.getDate()
+  var year = date.getFullYear()
+  
+  return month + " " + day + ", " + year
+}
+
+app.get('/:timestamp', (req, res) => {
+  
+  var timestamp = req.params.timestamp
+  timestamp = isNaN(timestamp) ? timestamp : Number(timestamp)
+  var date = new Date(timestamp).getTime()
+  var payLoad = { "unix": null, "natural": null }
+  
+  //check if the timestamp is a valid date
+  //0 is true, false otherwise
+  if(date > 0){
+    payLoad.unix = isNaN(timestamp) ? date/1000 : timestamp
+    payLoad.natural = isNaN(timestamp) ? timestamp : unix2natural(timestamp)
+  }
+  res.send(JSON.stringify(payLoad))
+
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(8080, () => { console.log("listening on port 8080") } )
+
